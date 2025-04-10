215ffcc2cbc09f6902f3814c5aeec9a90beb7fb7
Denis Semenyuk <dinozavr.independent228@gmail.com>
2025-04-10 20:58:18 +0500

    Прошел еще один урок

---
+++ jscoursetwo/script_five.js
@@ -0,0 +1,37 @@
+// 5. JavaScript условия if else. Тернарный оператор. Логические операторы. Оператор нулевого слияния.
+
+const year = 2025
+
+if(year === 2025) {
+    console.log("Ура!");
+} else if (year === 3000) {
+    console.log('Поздновато');
+} else {
+    console.log('Вот так вот');   
+}
+
+
+const price = 900;
+
+if(price === 1000) {
+    message = 'Нормальная цена, можно брать'
+} else if ( price < 1000) {
+    message = 'О, скидка даже есть, берем'
+} else {
+    message = 'Нифига дорого, не, спасибо'
+};
+
+console.log(message);
+
+// Тернарный оператор
+
+const priceTwo = price === 1900 ? 'Дорого' : 'Нормально'
+
+console.log(priceTwo)
+
+// Логические операторы
+
+// false || false === Или
+// true && true === И
+// !true, !false
+// Оператор нулевого сливания a ?? b
--- jscoursetwo/script_four.js
+++ jscoursetwo/script_four.js
@@ -1,46 +1,46 @@
 // 4. Математика в JavaScript. Арифметические операторы, операторы сравнения. Декремент и инкремент.
 
-console.log(2 + 2);
-console.log(2 - 2);
-console.log(2 * 2);
-console.log(2 / 2);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
 
 // Возведение в степень
 
-console.log(2 ** 10);
+// console.log(2 ** 10);
 
 // Оператор остатка от деления (обычно проверка на четность)
 
-console.log(13 % 5);
-console.log(5 % 2);
+// console.log(13 % 5);
+// console.log(5 % 2);
 
 // Унарные оператор
 
-console.log(-3);
+// console.log(-3);
 
 // В данном случае вместо Number можно использовать +
 
-console.log(typeof +"123") == console.log(Number("123"));
+// console.log(typeof +"123") == console.log(Number("123"));
 
-console.log(+"3" + +"5");
+// console.log(+"3" + +"5");
 
-const result = (1 + 2 + (-3)) ** +"4" / 5 ** 2;
+// const result = (1 + 2 + (-3)) ** +"4" / 5 ** 2;
 
-console.log(result);
+// console.log(result);
 
-let count = 1
+// let count = 1
 
-count = count + 2
-count += 2
+// count = count + 2
+// count += 2
 
 //Инкримент
 
-let count_two = 10
+// let count_two = 10
 
-count++
+// count++
 
 //Декременты 
 
-let count_3 = 10
+// let count_3 = 10
 
-count--
+// count--
