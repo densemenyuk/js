eaef3d5be617d19422cebc4cba4540a57f869e9a
Denis Semenyuk <dinozavr.independent228@gmail.com>
2025-04-12 16:41:58 +0500

    Изучаю тему функций

---
+++ jscoursetwo/script_seven.js
@@ -0,0 +1,60 @@
+//Функции в JavaScript. Область видимости. Параметры и аргументы. Значения по умолчанию. Return.
+
+// function logHello () {
+//     console.log('Hello');
+    
+// };
+
+// logHello();
+
+
+// const message = 'Глобальная переменная'
+
+// function logMessage (){
+//     const message = 'Локальная переменная'
+
+//     console.log(message)
+
+//     for (let i = 0; i < 3; i++) {
+//         const message = `Итерация №${i}`
+//         console.log(message);
+        
+//     }
+// }
+
+// logMessage()
+
+// console.log(message)
+
+// Область видимости переменных 
+
+// {
+//     const number = 1
+//     console.log(number);
+//     {
+//         const number = 2
+//         console.log(number);
+//         {
+//             const number = 2
+//             console.log(number);
+//         }
+//     }
+    
+    
+// }
+
+// console.log(number);
+
+
+// function logMessage (message, count) {
+//     for (let i = 0; i < count; i++){
+//         console.log(message);
+//     }
+// }
+
+// logMessage('Первое сообщение', 2)
+// logMessage('Второе сообщение', 5)
+// logMessage('Третье сообщение', 1)
+
+
+
--- jscoursetwo/script_six.js
+++ jscoursetwo/script_six.js
@@ -33,8 +33,8 @@
 
 // continue
 
-for (let i = 0; i < 10; i++) {
-    if (i % 2 === 0)
-        continue
-    console.log(i)
-}
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0)
+//         continue
+//     console.log(i)
+// }
