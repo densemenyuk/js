aca523cf3dd5a1a21525dd4a11ca495783c72de8
Denis Semenyuk <dinozavr.independent228@gmail.com>
2025-04-24 20:44:34 +0500

    Изучил методы для строк

--- jscoursetwo/script_13.js
+++ jscoursetwo/script_13.js
@@ -65,10 +65,64 @@ console.log(`Строка ${messageFormatted} имеет длину ${messageFor
 )
  */
 
-
 //Проверка начинается ли или оканчивается строка на указаные значения
 
-const message = 'Начало строки и конец строки'
+/**
+ * const message = 'Начало строки и конец строки'
 
 console.log(message.startsWith('Нач'))
 console.log(message.endsWith('оки'))
+ */
+
+// Способ получение подстроки из строки
+
+/**
+ * const str = "JavaScript";
+
+console.log(str.substring(4, 9));
+// в slice можно передать отрицательное значение
+console.log(str.slice(0, 5));
+console.log(str.slice(-6));
+ */
+
+// Метод repeat
+
+/**
+// const str = "JavaScript";
+
+console.log(str.repeat(2)) 
+*/
+
+// Замена слов в строке
+// Этот способ меняет только первое найденое слово
+// replaceAll меняет все значения
+
+/**
+ * const message = "Я изучаю бэкенд";
+
+console.log(message.replace("бэкенд", "фронтенд"));
+ */
+
+// Метод split позволяет разбить строку на массив
+
+/**
+ * const str = 'Hello, world!'
+
+console.log(str.split(','))
+ */
+
+// Задачка на методы строк
+
+const value = prompt('Введите ваше имя: ')
+
+const clearValue = value.trim().toLocaleLowerCase()
+
+if(clearValue.length === 0) {
+    alert('Ошибка! Введите корректные данные!')
+}
+
+if(clearValue.includes('админ')) {
+    alert('Ошибка! Такое имя использовать нельзя!')
+}
+
+
